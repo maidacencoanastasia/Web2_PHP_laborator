b7b6df713198eb3a74920f0ff9f72cc1db682701
Anastasia <anastasia.maidacenco@udc.es>
2020-09-03 15:21:28 +0300

    Initial commit

---
+++ index_classwork.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<title>Maidacenco</title>
+This is my exemple
+<?php
+$numbers = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+$sum = 0;
+foreach ($numbers as $index=>$value) {
+	if ($value % 2 != 0){
+  echo "$value <br>";
+		$sum = $sum + $value;}
+}
+echo 'sum: '. $sum;
+?>
+</html>
